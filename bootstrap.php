01b921f5a565e49913c9bf077b7e6eaf00a96273
weliton <welingtonzsilva@gmail.com>
2022-03-28 21:59:06 -0300

    estanciando objetos, request e response

--- bootstrap.php
+++ bootstrap.php
@@ -2,16 +2,18 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-    
+
+
 require __DIR__ . '/vendor/autoload.php';
 
 // criando a estacia de app
 $app = AppFactory::create();
 
 // habilitando erros
-$app->addRoutingMiddleware();
-$errorMiddleware = $app->addErrorMiddleware(true,true,true);
+// $app->addRoutingMiddleware();
+// $errorMiddleware = $app->addErrorMiddleware(true,true,true);
 
 // criando objetos 
 $response = Response::class;
 $request = Request::class;
+
