c53d1ad65de5044b41b78e6cd85a5507603332c2
weliton <welingtonzsilva@gmail.com>
2022-03-28 22:18:33 -0300

    implendando interfaceController

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,15 +2,16 @@
 
     namespace Weliton\TwigSlim\Controllers;
 
+use Weliton\TwigSlim\Traits\View;
 
-
-class HomeController extends Controller
+class HomeController implements InterfaceController
 {
+    use View;
     public function index():void
     {
         $this->view('home',[
             'nome' => 'weliton',
-            'titulo' => 'Home'
+            'titulo' => 'Welitinho piranhudo'
         ]);
 
     }
