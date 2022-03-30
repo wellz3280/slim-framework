d4fc820522f718ddd772288740ba7304eccb366b
weliton <welingtonzsilva@gmail.com>
2022-03-30 01:38:21 -0300

    rota para controller cadastro adicionada

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use Weliton\TwigSlim\Controllers\CadastroController;
 use Weliton\TwigSlim\Controllers\ContatoController;
 use Weliton\TwigSlim\Controllers\HomeController;
 use Weliton\TwigSlim\Controllers\UpdateController;
@@ -8,5 +9,7 @@ use Weliton\TwigSlim\Controllers\UpdateController;
 
         '/' => HomeController::class.':index',
         '/contato' => ContatoController::class.':index',
-        '/update/user/{id}'=> UpdateController::class.':update'
+        '/update/user/{id}'=> UpdateController::class.':update',
+        '/cadastro'=> CadastroController::class.':index',
+        '/cadastro/store'=> CadastroController::class.':store'
     ];
