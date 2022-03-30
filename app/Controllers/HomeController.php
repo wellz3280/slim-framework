d2292fb0ae1edcf3239fd09231b782ed34482865
weliton <welingtonzsilva@gmail.com>
2022-03-30 01:36:23 -0300

    testes com traits

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,15 +3,13 @@
     namespace Weliton\TwigSlim\Controllers;
 
 use Weliton\TwigSlim\model\Post;
+use Weliton\TwigSlim\Traits\Dump;
 
-use Weliton\TwigSlim\Traits\Load;
 use Weliton\TwigSlim\Traits\View;
 
 class HomeController implements InterfaceController
 {
-    use Load;
     use View;
-    
     public function index():void
     {
         $post = new Post();
